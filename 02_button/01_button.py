eff84f96f395dc6462382d6347538402aab77721
Ali Neaz <neazali77@gmail.com>
2021-09-27 01:20:22 +0600

    did not complete the button

---
+++ 02_button/01_button.py
@@ -0,0 +1,11 @@
+from tkinter import*
+
+root = Tk()
+
+root.title("abc company")
+root.geometry("500x400")
+lab = Label(root, text="Hello", font=("times new roman", 30, "bold"))
+lab.place(x=100, y=100)
+
+
+root.mainloop()
